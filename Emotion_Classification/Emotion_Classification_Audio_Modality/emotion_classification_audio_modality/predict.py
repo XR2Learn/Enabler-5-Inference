9413937b204ffea6ab0b51ebe22c3a5d623f8053
Annanda Sousa <annanda.sousa@gmail.com>
2023-10-09 11:56:04 +0200

    Facilitating local development - outputs/datasets/configuration folders and file.

--- Emotion_Classification/Emotion_Classification_Audio_Modality/emotion_classification_audio_modality/predict.py
+++ Emotion_Classification/Emotion_Classification_Audio_Modality/emotion_classification_audio_modality/predict.py
@@ -1,11 +1,12 @@
 # Write Python code here
-from emotion_classification_audio_modality.conf import DATASETS_FOLDER, OUTPUTS_FOLDER
+from emotion_classification_audio_modality.conf import DATASETS_FOLDER, OUTPUTS_FOLDER, PATH_CUSTOM_SETTINGS
 
 
 def testing_component():
     print(f'Running Docker for Emotion Classification - Audio Modality')
     print(DATASETS_FOLDER)
     print(OUTPUTS_FOLDER)
+    print(PATH_CUSTOM_SETTINGS)
 
 
 if __name__ == '__main__':
