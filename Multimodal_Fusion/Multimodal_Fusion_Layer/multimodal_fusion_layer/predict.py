620d4c37e7fd73e060976afc7b0f51fc801062d3
Annanda Sousa <annanda.sousa@gmail.com>
2023-10-10 10:29:03 +0200

    More architecture creation - including multimodal fusion layer.

---
+++ Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/predict.py
@@ -0,0 +1,9 @@
+# Python code here
+
+
+def testing_multimodal_layer():
+    print(f'Running Docker for Multimodal Layer!')
+
+
+if __name__ == '__main__':
+    testing_multimodal_layer()
---
+++ Multimodal_Fusion/Multimodal_Fusion_Layer/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='multimodal_fusion_layer',
+    version='0.1',
+    description='Multimodal Fusion Layer.',
+    author='UM-XR2Learn-Enablers',
+    packages=['multimodal_fusion_layer'],
+    zip_safe=False
+)
