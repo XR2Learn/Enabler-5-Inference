bd34e2e04789b1df1f0f4a971f2bd28635398f70
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-10-18 10:18:51 +0200

    seperate prediction and evaluation

--- ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
+++ ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
@@ -1,96 +1,18 @@
 # Write Python code here
 import pandas as pd
-import numpy as np
 import os
-import torch
 
-from tqdm import tqdm
-from conf import DATASETS_FOLDER, OUTPUTS_FOLDER, CUSTOM_SETTINGS,EXPERIMENT_ID,ID_TO_LABEL
-from utils.init_utils import (init_encoder)
-from classifiers.linear import LinearClassifier
-from classification_model import classification_model
-
-from sklearn.metrics import accuracy_score,classification_report
+from sklearn.metrics import classification_report
+from conf import OUTPUTS_FOLDER
 
 
 def evaluate():
     print(f'Running Docker for Emotion Classification - Audio Modality')
 
-    print(CUSTOM_SETTINGS)
-    splith_paths = {'test': "test.csv"}
-
-    encoder = init_encoder(CUSTOM_SETTINGS["encoder_config"],
-                           CUSTOM_SETTINGS['encoder_config']['pretrained'] if "pretrained_path" in CUSTOM_SETTINGS[
-                               'encoder_config'].keys()  else f"{OUTPUTS_FOLDER}/SSL_Training/{EXPERIMENT_ID}_encoder.pt" if "pretrained_same_experiment" in CUSTOM_SETTINGS[
-                               'encoder_config'].keys() and CUSTOM_SETTINGS['encoder_config']["pretrained_same_experiment"] else None
-                           )
-    encoder.eval()
-    classifier = LinearClassifier(encoder.out_size, CUSTOM_SETTINGS['dataset_config']['number_of_labels'])
-    classifier.eval()
-
-    model = classification_model(encoder=encoder, classifier=classifier, **CUSTOM_SETTINGS['sup_config']['kwargs'])
-    model.load_state_dict(torch.load(os.path.join(OUTPUTS_FOLDER,'supervised_training','dev_model_model.pt')))
-    model.eval()
-    print(model)
-
-    predict_and_save_full_model(model,splith_paths['test'],'')
-    predict_and_save_classifier(classifier,splith_paths['test'],'')
-
-def predict_and_save_full_model(model, csv_path, out_path):
-    """
-    generate_and_save : given the encoder, extract the features and save to .npy files
-
-    Args:
-        encoder: the pytorch encoder model to extract features from
-        csv_path: csv containing the paths to the files for which features have to be extracted and saved
-        out_path: output path to save the features to
-    Returns:
-        none
-    """
-
-    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, csv_path))
-    all_predictions = []
-    for data_path in tqdm(meta_data['files']):
-        # TODO : find replacement for .replace('\\','/')) to have a seperator that works on all OS
-        x = np.load(
-            os.path.join(OUTPUTS_FOLDER, CUSTOM_SETTINGS['encoder_config']['input_type'], data_path).replace('\\', '/'))
-        x_tensor = torch.tensor(np.expand_dims(x, axis=0) if len(x.shape) <= 1 else x)
-        prediction = model(x_tensor.T)
-        prediction_label = ID_TO_LABEL['RAVDESS'][torch.argmax(prediction).item()]
-        all_predictions.append(prediction_label)
-    meta_data['prediction'] = all_predictions
-    meta_data.to_csv(os.path.join(OUTPUTS_FOLDER,'predictions.csv'))
-        # print(data_path.split(os.path.sep))
-        #np.save(os.path.join(out_path, data_path.split(os.path.sep)[-1]), features.detach().numpy())
-    evaluate_predictions(meta_data['labels'],all_predictions)
-
-def predict_and_save_classifier(classifier, csv_path, out_path):
-    """
-    generate_and_save : given the encoder, extract the features and save to .npy files
-
-    Args:
-        encoder: the pytorch encoder model to extract features from
-        csv_path: csv containing the paths to the files for which features have to be extracted and saved
-        out_path: output path to save the features to
-    Returns:
-        none
-    """
-
-    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, csv_path))
-    all_predictions = []
-    for data_path in tqdm(meta_data['files']):
-        # TODO : find replacement for .replace('\\','/')) to have a seperator that works on all OS
-        x = np.load(
-            os.path.join(OUTPUTS_FOLDER,'SSL_features', data_path).replace('\\', '/'))
-        x_tensor = torch.tensor(np.expand_dims(x, axis=0) if len(x.shape) <= 1 else x)
-        prediction = classifier(torch.nn.Flatten(start_dim=0)(x_tensor))
-        prediction_label = ID_TO_LABEL['RAVDESS'][torch.argmax(prediction).item()]
-        all_predictions.append(prediction_label)
-    meta_data['prediction'] = all_predictions
-    meta_data.to_csv(os.path.join(OUTPUTS_FOLDER,'predictions.csv'))
-        # print(data_path.split(os.path.sep))
-        #np.save(os.path.join(out_path, data_path.split(os.path.sep)[-1]), features.detach().numpy())
-    evaluate_predictions(meta_data['labels'],all_predictions)
+    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER,'predictions.csv'))
+    gt = meta_data['labels']
+    predictions = meta_data['prediction']
+    evaluate_predictions(gt,predictions)
 
 def evaluate_predictions(gt,prediction):
     #print(accuracy_score(gt,prediction))
