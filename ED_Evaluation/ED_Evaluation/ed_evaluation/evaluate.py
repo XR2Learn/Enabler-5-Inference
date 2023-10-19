1c1c2cd33bbfc515c0ebbeb293e526d1c9509b6d
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-10-19 15:43:37 +0200

    printing of conusion matrix in cli

--- ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
+++ ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
@@ -1,8 +1,9 @@
 # Write Python code here
+import numpy as np
 import pandas as pd
 import os
 
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,confusion_matrix
 from conf import OUTPUTS_FOLDER
 
 
@@ -14,9 +15,24 @@ def evaluate():
     predictions = meta_data['prediction']
     evaluate_predictions(gt,predictions)
 
-def evaluate_predictions(gt,prediction):
-    #print(accuracy_score(gt,prediction))
-    print(classification_report(gt,prediction))
+def evaluate_predictions(gt,prediction):    
+    # Compute confusion matrix
+    unique_labels = gt.unique()
+    cm = confusion_matrix(gt, prediction, labels=unique_labels)
+    print("Confusion Matrix:")
+    print("Actual\\Predicted   ", end="")
+    for label in unique_labels:
+        print(f"{label:<10s}", end="")
+    print()  # New line
+    for i, row in enumerate(cm):
+        print(f"{unique_labels[i]:<10s}", end="")
+        for value in row:
+            print(f"{value:10d}", end="")
+        print()  # New line
+    
+
+
+    print(classification_report(gt,prediction,zero_division=np.nan))
 
 
 
