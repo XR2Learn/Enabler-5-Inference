c724184ca3ca437d7e9168b1a318c0763000a4aa
Annanda Sousa <annanda.sousa@gmail.com>
2023-10-26 17:02:47 +0200

    Refactoring.

--- ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
+++ ED_Evaluation/ED_Evaluation/ed_evaluation/evaluate.py
@@ -1,21 +1,21 @@
 # Write Python code here
-import numpy as np
-import pandas as pd
 import os
 
-from sklearn.metrics import classification_report,confusion_matrix
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+
 from conf import OUTPUTS_FOLDER
 
 
 def evaluate():
-    print(f'Running Docker for Emotion Classification - Audio Modality')
-
-    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER,'predictions.csv'))
+    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, 'predictions.csv'))
     gt = meta_data['labels']
     predictions = meta_data['prediction']
-    evaluate_predictions(gt,predictions)
+    evaluate_predictions(gt, predictions)
 
-def evaluate_predictions(gt,prediction):    
+
+def evaluate_predictions(gt, prediction):
     # Compute confusion matrix
     unique_labels = gt.unique()
     cm = confusion_matrix(gt, prediction, labels=unique_labels)
@@ -29,13 +29,8 @@ def evaluate_predictions(gt,prediction):
         for value in row:
             print(f"{value:10d}", end="")
         print()  # New line
-    
 
-
-    print(classification_report(gt,prediction,zero_division=np.nan))
-
-
-
+    print(classification_report(gt, prediction, zero_division=np.nan))
 
 
 if __name__ == '__main__':
--- Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/predict.py
+++ Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/predict.py
@@ -1,43 +1,47 @@
 import os
+
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from conf import DATASETS_FOLDER, OUTPUTS_FOLDER, CUSTOM_SETTINGS,ID_TO_LABEL
 
+from conf import OUTPUTS_FOLDER, CUSTOM_SETTINGS, ID_TO_LABEL
 
 
 def testing_multimodal_layer():
-    #print(f'Running Docker for Multimodal Layer!')
-    #modalities = ['eGeMAPs','MFCC']
-    #weights = [0.6,0.4]
+    # modalities = ['eGeMAPs','MFCC']
+    # weights = [0.6,0.4]
     modalities = [CUSTOM_SETTINGS["encoder_config"]["input_type"]]
-    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER,'test.csv'))
+    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, 'test.csv'))
     files = meta_data['files']
     all_predictions = []
     for f in tqdm(files):
-        all_predictions_for_file = extract_predictions(modalities,f)
+        all_predictions_for_file = extract_predictions(modalities, f)
         majority_index = get_majority_voting_index(all_predictions_for_file)
         prediction_label = ID_TO_LABEL['RAVDESS'][majority_index]
-        #print(prediction_label)
+        # print(prediction_label)
         all_predictions.append(prediction_label)
     meta_data['prediction'] = all_predictions
-    meta_data.to_csv(os.path.join(OUTPUTS_FOLDER,'predictions.csv'))
+    meta_data.to_csv(os.path.join(OUTPUTS_FOLDER, 'predictions.csv'))
 
 
-def extract_predictions(modalities,filename):
+def extract_predictions(modalities, filename):
     all_predictions = None
     for mod in modalities:
-        single_prediction = np.load(os.path.join(OUTPUTS_FOLDER,f"prediction-{mod}",filename))
-        all_predictions = single_prediction if all_predictions is None else np.vstack((all_predictions,single_prediction))
+        single_prediction = np.load(os.path.join(OUTPUTS_FOLDER, f"prediction-{mod}", filename))
+        all_predictions = single_prediction if all_predictions is None else np.vstack(
+            (all_predictions, single_prediction))
     return all_predictions
 
-def get_majority_voting_index(predictions):
-    if len(predictions.shape)<=1:
-        return np.argmax(predictions)
-    return np.argmax(np.sum(predictions,axis=0))
 
-def get_majority_voting_index_with_weights(predictions,weights):
-    return np.argmax(np.dot(weights,predictions))
+def get_majority_voting_index(predictions):
+    if len(predictions.shape) <= 1:
+        return np.argmax(predictions)
+    return np.argmax(np.sum(predictions, axis=0))
+
+
+def get_majority_voting_index_with_weights(predictions, weights):
+    return np.argmax(np.dot(weights, predictions))
+
 
 if __name__ == '__main__':
     testing_multimodal_layer()
