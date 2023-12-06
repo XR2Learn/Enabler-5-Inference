32a8db5c3b772a0b35d439b0c126e9a071385539
Annanda Sousa <annanda.sousa@gmail.com>
2023-12-06 15:48:41 +0100

    Bump version: 0.1.0 → 0.1.1

--- Emotion_Classification/Emotion_Classification_Audio_Modality/setup.py
+++ Emotion_Classification/Emotion_Classification_Audio_Modality/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='emotion_classification_audio_modality',
-    version='0.1.0',
+    version='0.1.1',
     description='Emotion Classification using audio modality.',
     author='UM-XR2Learn-Enablers',
     packages=['emotion_classification_audio_modality'],
