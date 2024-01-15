5bec088c5a4cab7f53c87cddc277e7ab7ab9f120
Annanda Sousa <annanda.sousa@gmail.com>
2024-01-15 12:12:09 +0100

    Making multimodal layer fusion a publisher of the predicted emotion.

--- Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/predict.py
+++ Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/predict.py
@@ -14,20 +14,9 @@ def multimodal_prediction():
     meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, 'test.csv'))
 
     if CUSTOM_SETTINGS['inference_config'].get('publisher', False):
-        publish_predicted_emotion()
+        publish_predicted_emotion(meta_data, modalities)
     else:
         write_predicted_emotion(meta_data, modalities)
-
-    # files = meta_data['files']
-    # all_predictions = []
-    # for f in tqdm(files):
-    #     all_predictions_for_file = extract_predictions(modalities, f)
-    #     majority_index = get_majority_voting_index(all_predictions_for_file)
-    #     prediction_label = ID_TO_LABEL['RAVDESS'][majority_index]
-    #     # print(prediction_label)
-    #     all_predictions.append(prediction_label)
-    # meta_data['prediction'] = all_predictions
-    # meta_data.to_csv(os.path.join(OUTPUTS_FOLDER, 'predictions.csv'))
 
 
 def write_predicted_emotion(meta_data, modalities):
@@ -43,8 +32,14 @@ def write_predicted_emotion(meta_data, modalities):
     meta_data.to_csv(os.path.join(OUTPUTS_FOLDER, 'predictions.csv'))
 
 
-def publish_predicted_emotion():
-    pass
+def publish_predicted_emotion(meta_data, modalities):
+    files = meta_data['files']
+    print('Publishing the predicted emotions')
+    for file in files:
+        all_predictions_for_file = extract_predictions(modalities, file)
+        majority_index = get_majority_voting_index(all_predictions_for_file)
+        prediction_label_to_publish = ID_TO_LABEL['RAVDESS'][majority_index]
+        print(prediction_label_to_publish)
 
 
 def extract_predictions(modalities, filename):
