bc4974d1c8de45996fd3c33b21956ecd66629c20
Annanda Sousa <annanda.sousa@gmail.com>
2024-01-15 15:40:28 +0100

    Making the publisher to subscribe the next activity level channel, to print the result for dev.

--- Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/emotion_publisher.py
+++ Multimodal_Fusion/Multimodal_Fusion_Layer/multimodal_fusion_layer/emotion_publisher.py
@@ -11,8 +11,8 @@ class EmotionPublisher:
     def __init__(self, redis_cli):
         self.redis_cli = redis_cli
         self.pubsub = self.redis_cli.pubsub()
-        # self.sub_event_types = {'next_activity_level': self.handle_next_activity_level,
-        #                         'debug_considered_emotions': self.handle_debug_considered_emotions}
+        self.sub_event_types = {'next_activity_level': self.handle_next_activity_level
+                                }
 
     def publish_emotion(self, label):
         event_type = 'emotion'
@@ -20,19 +20,47 @@ class EmotionPublisher:
             'emotion': label
         }
         print(event_data)
-        print(self.publish_activity(event_type, event_data))
+        self.publish_activity(event_type, event_data)
+        # Just for dev
         time.sleep(2)
+
+    def start_activity(self):
+        self.subscribe_suggested_activity_level()
+        event_type = 'start_activity'
+        event_data = {
+            'id': 0,
+            'user_level': 0,
+            'activity_level': randint(0, 50) % 3
+        }
+        self.publish_activity(event_type, event_data)
+
+    def end_activity(self):
+        event_type = 'end_activity'
+        event_data = {
+            'id': 0,
+            'timestamp': time.time()  # 1 is flow
+        }
+        self.publish_activity(event_type, event_data)
 
     def publish_activity(self, event_type, event_data):
         json_message = json.dumps(event_data)
         result = self.redis_cli.publish(event_type, json_message)
         return result
 
+    def subscribe_suggested_activity_level(self):
+        self.pubsub.subscribe(**self.sub_event_types)
+        self.sub_thread = self.pubsub.run_in_thread(sleep_time=0.001)
+
+    def handle_next_activity_level(self, message):
+        print(message['data'])
+        self.sub_thread.stop()
+
 
 if __name__ == '__main__':
-    pass
-    # redis_cli = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-    #
-    # emotion_publisher = EmotionPublisher(redis_cli)
-    # for _ in range(3):
-    #     emotion_publisher.run()
+    redis_cli = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+
+    emotion_publisher = EmotionPublisher(redis_cli)
+    time.sleep(5)
+    emotion_publisher.start_activity()
+    time.sleep(20)
+    emotion_publisher.end_activity()
