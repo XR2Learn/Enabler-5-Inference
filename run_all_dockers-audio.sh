aa1c0e0371e6896f33535796ea25062412293e2d
Annanda Sousa <annanda.sousa@gmail.com>
2024-04-08 14:38:19 +0200

    Include config file path to the script to run all dockers.

--- run_all_dockers-audio.sh
+++ run_all_dockers-audio.sh
@@ -3,14 +3,14 @@
 echo "--------------------"
 echo "Emotion-classification-audio"
 echo "--------------------"
-docker compose run --rm emotion-classification-audio
+CONFIG_FILE_PATH=$CONFIG_FILE_PATH docker compose run --rm emotion-classification-audio
 echo "--------------------"
 echo "Fusion-layer"
 echo "--------------------"
-docker compose run --rm fusion-layer
+CONFIG_FILE_PATH=$CONFIG_FILE_PATH docker compose run --rm fusion-layer
 echo "--------------------"
 echo "Ed-evaluation"
 echo "--------------------"
-docker compose run --rm ed-evaluation
+CONFIG_FILE_PATH=$CONFIG_FILE_PATH compose run --rm ed-evaluation
 echo "--------------------"
 
