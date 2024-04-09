e29b57147f4205f3325b17dc593ca0d13153362f
Annanda Sousa <annanda.sousa@gmail.com>
2024-04-09 16:40:12 +0200

    Adding emotion classification component for the body tracking modality.

---
+++ Emotion_Classification/Emotion_Classification_Body_Tracking_Modality/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='emotion_classification_body_tracking_modality',
+    version='0.4.0',
+    description='Emotion Classification component for body tracking modality.',
+    author='UM-XR2Learn-Enablers',
+    packages=['emotion_classification_body_tracking_modality'],
+    zip_safe=False
+)
