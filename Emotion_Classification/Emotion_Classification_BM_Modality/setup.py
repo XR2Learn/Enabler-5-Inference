79529e6bdd08a32ea893654b45725f6a7a714033
Annanda Sousa <annanda.sousa@gmail.com>
2024-07-24 11:46:22 +0200

    Bump version: 0.5.0 → 0.6.0

--- Emotion_Classification/Emotion_Classification_BM_Modality/setup.py
+++ Emotion_Classification/Emotion_Classification_BM_Modality/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='emotion_classification_bm_modality',
-    version='0.5.0',
+    version='0.6.0',
     description='Emotion Classification using bm modality',
     author='UM-XR2Learn-Enablers',
     packages=['emotion_classification_bm_modality'],
--- Mock_XRoom_Writer/Mock_XRoom_Writer_BM_Modality/setup.py
+++ Mock_XRoom_Writer/Mock_XRoom_Writer_BM_Modality/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mock_xroom_writer_bm_modality',
-    version='0.5.0',
+    version='0.6.0',
     description='Mock XRoom bm modality',
     author='UM-XR2Learn-Enablers',
     packages=['mock_xroom_writer_bm_modality'],
