1ae3093d4f9559e627e8a3695292bc3d23a4e5ce
bulatkh <bulatkhaert@gmail.com>
2024-08-10 19:17:27 +0200

    Update mock writer to simulate two modalities

--- Mock_XRoom_Writer/Mock_XRoom_Writer_BM_Modality/mock_xroom_writer_bm_modality/mock_xroom_writer.py
+++ Mock_XRoom_Writer/Mock_XRoom_Writer_BM_Modality/mock_xroom_writer_bm_modality/mock_xroom_writer.py
@@ -1,5 +1,6 @@
 import time
 import csv
+import itertools
 import os
 import pathlib
 
@@ -10,33 +11,38 @@ def read_csv(input_file):
         for row in reader:
             yield row
 
-
 def write_csv(output_file, data):
     with open(output_file, 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(data)
 
 
-def main(input_file, output_file):
-    if not os.path.exists(output_file):
-        # Create the output CSV file if it does not exist
-        with open(output_file, 'w', newline='') as _:
-            pass  # Empty file
-
-    for row in read_csv(input_file):
-        if row:
-            write_csv(output_file, row)
-            time.sleep(0.1)  # Reading delay
+def main(input_files, output_files):
+    for output_file in output_files:
+        if not os.path.exists(output_file):
+            with open(output_file, 'w', newline='') as _:
+                pass  # Empty file
+    
+    for row1, row2 in tuple(itertools.zip_longest(read_csv(input_files[0]), read_csv(input_files[1]))):
+        if row1:
+            write_csv(output_files[0], row1)
+        if row2:
+            write_csv(output_files[1], row2)    
+        time.sleep(0.1)  # Reading delay
 
 
 if __name__ == "__main__":
     input_files = [
-        "./datasets/XRoom/V1_BK1/data_collection_638461160214938655_SHIMMER_.csv",
-        "./datasets/XRoom/V1_BK1/data_collection_638461162609580603_SHIMMER_.csv"
+        ["./datasets/XRoom/V1_BK1/data_collection_638461160214938655_VR_.csv", "./datasets/XRoom/V1_BK1/data_collection_638461160214938655_SHIMMER_.csv"],
+        ["./datasets/XRoom/V1_BK1/data_collection_638461162609580603_VR_.csv", "./datasets/XRoom/V1_BK1/data_collection_638461162609580603_SHIMMER_.csv"]
     ]
     MAIN_FOLDER_DEFAULT = pathlib.Path(__file__).parent.parent.absolute()
     output_folder = os.path.join(MAIN_FOLDER_DEFAULT, 'datasets', 'test_data')
-    # output_folder = './test_data'
-    output_files = [os.path.join(output_folder, os.path.basename(input_file)) for input_file in input_files]
+    output_files = [
+        [
+            os.path.join(output_folder, os.path.basename(input_file[i]))
+            for i in range(len(input_file))
+        ]  for input_file in input_files
+    ]
     for input_file, output_file in zip(input_files, output_files):
         main(input_file, output_file)
