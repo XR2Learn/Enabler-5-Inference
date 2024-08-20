64730d1cbf9059c44a21c8becfe9cde7b824f06d
Annanda Sousa <annanda.sousa@gmail.com>
2024-08-20 13:29:26 +0200

    Body tracking inference (non-streaming) not supported.

---
+++ run_all_dockers-body-tracking.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Deleting All dataset, output files
+
+echo "--------------------"
+echo "Emotion-classification-bm"
+echo "--------------------"
+docker compose run --rm emotion-classification-body-tracking
+echo "--------------------"
+echo "Fusion-layer"
+echo "--------------------"
+docker compose run --rm fusion-layer
+echo "--------------------"
+echo "Ed-evaluation"
+echo "--------------------"
+docker compose run --rm ed-evaluation
+echo "--------------------"
+
