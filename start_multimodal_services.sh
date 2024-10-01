30fb549d1990de4cd3dfb3e132daada6aa120e04
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-01 10:51:32 +0200

    Refactoring to use the new configuration structure.

--- start_multimodal_services.sh
+++ start_multimodal_services.sh
@@ -4,13 +4,13 @@
 echo "--------------------"
 docker compose start dashboard
 echo "--------------------"
-CONFIG_FILE_PATH="./configs/example.configuration_bm_end2end_streaming.json" docker compose up emotion-classification-bm -d
+CONFIG_FILE_PATH="./configs/example.configuration_multimodal_bm_body_tracking_streaming.json" docker compose up emotion-classification-bm -d
 echo "--------------------"
-CONFIG_FILE_PATH="./configs/example.configuration_body_tracking_end2end_streaming.json" docker compose up emotion-classification-body-tracking -d
+CONFIG_FILE_PATH="./configs/example.configuration_multimodal_bm_body_tracking_streaming.json" docker compose up emotion-classification-body-tracking -d
 echo "--------------------"
-CONFIG_FILE_PATH="./configs/example.configuration_bm_end2end_streaming.json" docker compose up inference-data-processing-bm -d
+CONFIG_FILE_PATH="./configs/example.configuration_multimodal_bm_body_tracking_streaming.json" docker compose up inference-data-processing-bm -d
 echo "--------------------"
-CONFIG_FILE_PATH="./configs/example.configuration_body_tracking_end2end_streaming.json" docker compose up inference-data-processing-body-tracking -d
+CONFIG_FILE_PATH="./configs/example.configuration_multimodal_bm_body_tracking_streaming.json" docker compose up inference-data-processing-body-tracking -d
 echo "--------------------"
-CONFIG_FILE_PATH="./configs/example.configuration_body_tracking_end2end_streaming.json" docker compose up fusion-layer -d
+CONFIG_FILE_PATH="./configs/example.configuration_multimodal_bm_body_tracking_streaming.json" docker compose up fusion-layer -d
 docker compose up mock-xroom-writer -d
